48dccb5905f8d7c2da2f7e972697c77ba413e57b
Pedro A. González <pgonzalez@post.com>
2022-12-06 21:02:21 -0500

    Add first lecture block of code, counter example

--- m07w18/my-app/src/App.js
+++ m07w18/my-app/src/App.js
@@ -1,7 +1,25 @@
+import { useState, useEffect } from 'react';
+
 import './App.css';
 
 function App() {
-  return <h1>Hello from App! 👋</h1>;
+  const [counter, setCounter] = useState(0);
+
+  const handleDecrement = () => setCounter(counter - 1);
+  const handleIncrement = () => setCounter(counter + 1);
+
+  useEffect(() => {
+    console.log('This is a side effect apart from rendering!');
+    document.title = `Your count: ${counter}`;
+  }, [counter]);
+
+  return (
+    <div>
+      <button onClick={handleDecrement}>Decrement</button>
+      <p>Your count: {counter}</p>
+      <button onClick={handleIncrement}>Increment</button>
+    </div>
+  );
 }
 
 export default App;
