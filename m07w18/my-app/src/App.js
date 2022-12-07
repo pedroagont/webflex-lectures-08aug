62c28c7b1d2deef96cd108ccd38550466811664f
Pedro A. González <pgonzalez@post.com>
2022-12-06 22:12:12 -0500

    Add second block of code for data fetching lecture

--- m07w18/my-app/src/App.js
+++ m07w18/my-app/src/App.js
@@ -4,6 +4,9 @@ import './App.css';
 
 function App() {
   const [counter, setCounter] = useState(0);
+  const [shibaImg, setShibaImg] = useState(
+    'https://media.tenor.com/FawYo00tBekAAAAC/loading-thinking.gif'
+  );
 
   const handleDecrement = () => setCounter(counter - 1);
   const handleIncrement = () => setCounter(counter + 1);
@@ -13,12 +16,31 @@ function App() {
     document.title = `Your count: ${counter}`;
   }, [counter]);
 
+  useEffect(() => {
+    const changeImgInterval = setInterval(() => {
+      fetch('https://shibe.online/api/shibes')
+        .then((res) => res.json())
+        .then((data) => setShibaImg(data[0]));
+    }, 2000);
+
+    const cleanUp = () => {
+      clearInterval(changeImgInterval);
+    };
+
+    return cleanUp;
+  }, []);
+
   return (
-    <div>
-      <button onClick={handleDecrement}>Decrement</button>
-      <p>Your count: {counter}</p>
-      <button onClick={handleIncrement}>Increment</button>
-    </div>
+    <>
+      <div style={{ maxWidth: '200px' }}>
+        <img src={shibaImg} alt="shiba inu" width="100%" />
+      </div>
+      <div>
+        <button onClick={handleDecrement}>Decrement</button>
+        <p>Your count: {counter}</p>
+        <button onClick={handleIncrement}>Increment</button>
+      </div>
+    </>
   );
 }
 
